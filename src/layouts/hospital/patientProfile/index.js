bdd2a55a7f9694cd50c1e95239cc92d041996665
Dennis Mathew <dennis@zdistancelab.com>
2023-05-31 04:03:05 +0530

    patient profile added

--- src/layouts/hospital/patientProfile/index.js
+++ src/layouts/hospital/patientProfile/index.js
@@ -1,43 +1,27 @@
 // @mui material components
 import Grid from "@mui/material/Grid";
-import Card from "@mui/material/Card";
-
-// @mui icons
-import FacebookIcon from "@mui/icons-material/Facebook";
-import TwitterIcon from "@mui/icons-material/Twitter";
-import InstagramIcon from "@mui/icons-material/Instagram";
-
 // Argon Dashboard 2 MUI components
 import ArgonBox from "components/ArgonBox";
-import ArgonTypography from "components/ArgonTypography";
 
 // Argon Dashboard 2 MUI example components
 import DashboardLayout from "examples/LayoutContainers/DashboardLayout";
 import Footer from "examples/Footer";
-import ProfileInfoCard from "examples/Cards/InfoCards/ProfileInfoCard";
-import ProfilesList from "examples/Lists/ProfilesList";
-import DefaultProjectCard from "examples/Cards/ProjectCards/DefaultProjectCard";
-import PlaceholderCard from "examples/Cards/PlaceholderCard";
+import ProfileInfoCard from "./ProfileInfoCard";
 
 // Overview page components
 import Header from "layouts/hospital/patientProfile/components/Header";
-import PlatformSettings from "layouts/profile/components/PlatformSettings";
 
 // Data
-import profilesListData from "layouts/profile/data/profilesListData";
+import profilesListData from "layouts/hospital/patientProfile/data/HospitalListData";
+import HospitalInsuranceList from "./hospitalInsuranceList";
 
-// Images
-import homeDecor1 from "assets/images/home-decor-1.jpg";
-import homeDecor2 from "assets/images/home-decor-2.jpg";
-import homeDecor3 from "assets/images/home-decor-3.jpg";
-import team1 from "assets/images/team-1.jpg";
-import team2 from "assets/images/team-2.jpg";
-import team3 from "assets/images/team-3.jpg";
-import team4 from "assets/images/team-4.jpg";
+
+
 const bgImage =
   "https://images.pexels.com/photos/207601/pexels-photo-207601.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1";
 
 function Overview() {
+
   return (
     <DashboardLayout
       sx={{
@@ -55,35 +39,19 @@ function Overview() {
           <Grid item xs={12} md={6} xl={4}>
             <ProfileInfoCard
               title="profile information"
-              description="Hi, I'm Alec Thompson, Decisions: If you can't decide, the answer is no. If two equally difficult paths, choose the one more painful in the short term (pain avoidance is creating an illusion of equality)."
               info={{
-                fullName: "Alec M. Thompson",
-                mobile: "(44) 123 1234 123",
-                email: "alecthompson@mail.com",
-                location: "USA",
+                fullName: "Shef rehmon",
+                mobile: "9988776655",
+                email: "rehmon@mail.com",
+                gender:"male",
+                dob:"01-01-0001",
+                bloodgroup:"rarest of rarest",
+                location: "Kazhakuttam",
               }}
-              social={[
-                {
-                  link: "https://www.facebook.com/CreativeTim/",
-                  icon: <FacebookIcon />,
-                  color: "facebook",
-                },
-                {
-                  link: "https://twitter.com/creativetim",
-                  icon: <TwitterIcon />,
-                  color: "twitter",
-                },
-                {
-                  link: "https://www.instagram.com/creativetimofficial/",
-                  icon: <InstagramIcon />,
-                  color: "instagram",
-                },
-              ]}
-              action={{ route: "", tooltip: "Edit Profile" }}
             />
           </Grid>
           <Grid item xs={12} xl={8}>
-            <ProfilesList title="conversations" profiles={profilesListData} />
+            <HospitalInsuranceList title="Assigned Hospitals" list={profilesListData} />
           </Grid>
         </Grid>
       </ArgonBox>
@@ -91,5 +59,4 @@ function Overview() {
     </DashboardLayout>
   );
 }
-
 export default Overview;
